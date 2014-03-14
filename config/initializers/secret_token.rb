b1822dff03e56ddbec3ff68e382a8cc342fa5a57
Charles Duprey <barbatos@urbanterror.info>
2014-03-14 14:12:06 +0100

    Hello Ruby World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarbatosFr::Application.config.secret_token = 'eb973591646c905a38846c6776898690767656e666fb6bf751b4014b38654219cf6c2f79ce40f1c5799c0a80c64e1d1173b1f4eee2bf98744aa3a8f672139f7d'
